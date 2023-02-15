8c41cc6c7a3eb3cd677a3dcba1c6a5db207a554e
Miller1999 <millerjavierarias@gmail.com>
2023-02-14 23:26:52 -0500

    Segunda clase

--- Program.cs
+++ Program.cs
@@ -16,6 +16,11 @@ namespace restaurant10TablesReservation
             Console.WriteLine("Welcome to the best restaurant in the world");
             while (true)
             {
+                if(arrayCurrentIndex == 10)
+                {
+                    Console.WriteLine("The restaurant is full, try again next year");
+                    Environment.Exit(0);
+                }
                 Console.WriteLine("Are you registered user? Write true, or false to register");
                 userType = Convert.ToBoolean(Console.ReadLine());
                 if(userType == true)
@@ -23,6 +28,22 @@ namespace restaurant10TablesReservation
                     Console.WriteLine("Hello, you are a registered user, please enter your user name");
                     string userToSearch = Console.ReadLine();
                     Console.WriteLine("The user you searched is {0}", userToSearch);
+                    int index = Array.IndexOf(userNames, userToSearch);
+                    if (index == -1)
+                    {
+                        Console.WriteLine("User not found, try again or register");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Welcome {0}, it's a pleasure to give you food", userNames[index]);
+                    }
+                }
+                else if(userType == false)
+                {
+                    Console.WriteLine("please write and remember your User Name");
+                    userNames[arrayCurrentIndex] = Console.ReadLine();
+                    Console.WriteLine("Your user has been successfully\n" + "your user Name is {0}", userNames[arrayCurrentIndex]);
+                    arrayCurrentIndex++;
                 }
             }
         }
